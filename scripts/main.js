7b1165ea6ed91bc58cad4123a72428d005fc206c
Manish Kumar <manish.bb.kumar@oracle.com>
2024-02-15 14:05:09 +0530

    Updating path

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 requirejs.config({
   paths: {
-    'pouchdb': '../libs/offline-persistence-toolkit/dist/debug/pouchdb-browser-7.2.2',
-    'pouchfind': '../libs/offline-persistence-toolkit/dist/debug/pouchdb.find',
-    'persist': '../libs/offline-persistence-toolkit/dist/debug'
+    'pouchdb': '../../libs/offline-persistence-toolkit/dist/debug/pouchdb-browser-7.2.2',
+    'pouchfind': '../../libs/offline-persistence-toolkit/dist/debug/pouchdb.find',
+    'persist': '../../libs/offline-persistence-toolkit/dist/debug'
   }
 });
 requirejs(["app"]);
